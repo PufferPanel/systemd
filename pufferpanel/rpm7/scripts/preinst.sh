88edf92f8bd103ce74ffa9e18c57f3e40d1e0982
Joshua Taylor <lordralex@gmail.com>
2020-01-03 15:00:25 -0600

    Update preinst.sh

--- pufferpanel/rpm7/scripts/preinst.sh
+++ pufferpanel/rpm7/scripts/preinst.sh
@@ -2,4 +2,4 @@
 
 useradd --system --home /etc/pufferpanel --user-group pufferpanel
 mkdir -p /etc/pufferpanel /var/log/pufferpanel
-chown pufferpanel:pufferpanel /etc/pufferd /var/log/pufferd
+chown pufferpanel:pufferpanel /etc/pufferd /var/log/pufferpanel
